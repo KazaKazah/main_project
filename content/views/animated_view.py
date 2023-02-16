60ca7da0e869717220265a0645140d58fd8c25b1
asan <elt.8155@gmail.com>
2023-02-16 14:35:26 +0100

    1

--- content/views/animated_view.py
+++ content/views/animated_view.py
@@ -6,6 +6,7 @@ from content.models import *
 class AnimationHomePage(ListView):
     model = AnimationsName
     template_name = 'main/content/animations/home_anima.html'
+    paginate_by = 6
 
     def get_context_data(self, *args, **kwargs):
         anima = AnimationsName.objects.all()
