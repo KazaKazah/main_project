60fa8d8f38c542c3c8f090504b791abaee562631
KazaKazah <elt.8155@gmail.com>
2023-02-16 11:18:37 +0600

    1

--- content/models.py
+++ content/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django_s3_storage.storage import S3Storage
 
+storage = S3Storage(aws_s3_bucket_name='test-buckets-ny')
 # Create your models here.
 
 
-class Animations(models.Model):
-    name = models.Charfield
+class AnimationsName(models.Model):
+    name = models.CharField(max_length=255, default='', verbose_name='название анимации')
+    title = models.CharField(max_length=255, default='', verbose_name='краткое описание')
+    animat_pic = models.ImageField(storage=storage, blank=True, null=True)
+
