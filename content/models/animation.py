52b412cd394f8300e01639d235257d89fd6d5774
KazaKazah <elt.8155@gmail.com>
2023-02-18 23:36:00 +0600

    1

--- content/models/animation.py
+++ content/models/animation.py
@@ -9,13 +9,12 @@ storage = S3Storage(aws_s3_bucket_name='test-buckets-ny')
 
 class Animations(models.Model):
     name = models.CharField(max_length=255, default='', verbose_name='название анимации')
-    title = models.CharField(max_length=255, default='', verbose_name='краткое описание')
-    animat_pic = models.ImageField(storage=storage, blank=True, null=True, verbose_name='картника')
+    body = models.TextField(default='', verbose_name='краткое описание')
     url = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'анимационное'
-        verbose_name_plural = 'анимациоенное'
+        verbose_name = 'жанр'
+        verbose_name_plural = 'жанры'
--- content/views/animated_view.py
+++ content/views/animated_view.py
@@ -8,7 +8,7 @@ from content.models import *
 class AnimationHomePage(ListView):
     model = Animations
     template_name = 'main/content/animations/home_anima.html'
-    paginate_by = 3
+    paginate_by = 6
 
     def get_context_data(self, *args, **kwargs):
         anima = Animations.objects.all()
