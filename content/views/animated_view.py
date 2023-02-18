4ba003a236ef6ed6664a5f1674302f26f3d3cd08
KazaKazah <elt.8155@gmail.com>
2023-02-18 10:50:48 +0600

    1

--- content/views/animated_view.py
+++ content/views/animated_view.py
@@ -3,9 +3,9 @@ from content.models import *
 
 
 class AnimationHomePage(ListView):
-    paginate_by = 3
     model = AnimationsName
     template_name = 'main/content/animations/home_anima.html'
+    paginate_by = 3
 
     def get_context_data(self, *args, **kwargs):
         anima = AnimationsName.objects.all()
