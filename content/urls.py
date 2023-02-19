4f93c66f618a307456fcaffc7858980d573f1521
KazaKazah <elt.8155@gmail.com>
2023-02-19 17:05:43 +0600

    Update urls.py

--- content/urls.py
+++ content/urls.py
@@ -5,4 +5,5 @@ from content.views.animated_view import *
 urlpatterns = [
     path('content/', AnimationHomeView.as_view(), name='anima_home'),
     path('content/detail/<slug:slug>/', AnimeTypeDetailView.as_view(), name='anima_detail'),
+    path('content/detail/<slug:slug>/animations', AnimeAnimaDetailView.as_view(), name='anima_anima_detail'),
 ]
