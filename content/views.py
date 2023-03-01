7d43463fc02e696b9b6355d56649d461a5534f45
asan <elt.8155@gmail.com>
2023-03-01 10:49:34 +0100

    1

--- content/views.py
+++ content/views.py
@@ -20,7 +20,7 @@ class ContentDetailView(DetailView):
     template_name = 'main/content/detail_page.html'
 
     def get_context_data(self, *args, **kwargs):
-        cont = Content.objects.filter(=kwargs.get('object').id)
+        cont = Content.objects.filter(contentyp_id=kwargs.get('object').id)
         context = {
             'cont': cont
         }
