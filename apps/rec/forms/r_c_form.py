07fa702b7256985d9f2e1db0394bfcf6b0be383f
KazaKazah <elt.8155@gmail.com>
2023-03-05 01:37:43 +0600

    1

--- apps/rec/forms/r_c_form.py
+++ apps/rec/forms/r_c_form.py
@@ -6,10 +6,11 @@ from apps.rec.models import *
 class RCForm(forms.ModelForm):
     class Meta:
         model = Ret
-        fields = ('name', 'title_con', 'slug', 'cont_pic')
+        fields = ('name', 'title_con', 'slug', 'cont_pic', 'rrrr')
 
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'title_con': forms.TextInput(attrs={'class': 'form-control'}),
             'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'rrrr': forms.Select(attrs={'class': 'form-control'}),
         }
